4209689e8f8629be91b8d9109e9b196313807182
MNFI13-blode <naufaltaurus24@gmail.com>
2023-05-02 01:26:59 +0700

    Menambahkan Library

---
+++ tpmodul10_1302213131/main_method/Program.cs
@@ -0,0 +1,9 @@
+﻿using tpmodul10_1302213131;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double persamaan1 = Perhitungan.AkarPersamaan({1,-3,-10});
+        double persamaan2 = Perhitungan.HasilKuadrat({2,-3})
+    }
+}
---
+++ tpmodul10_1302213131/tpmodul10_1302213131/Perhitungan.cs
@@ -0,0 +1,22 @@
+﻿namespace tpmodul10_1302213131
+{
+    public class Perhitungan
+    {
+        public static (double,double) AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double x1,x2;
+            double akar = Math.Sqrt((persamaan[1] * persamaan[1]) - (4 * persamaan[0] * persamaan[2]));
+            x1 = -persamaan[1] + akar / (2 * persamaan[0]);
+            x2 = -persamaan[1] - akar / (2 * persamaan[0]);
+            return (x1,x2);
+        }
+        public static (double,double,double) HasilKuadrat(double[] persamaan)
+        {
+            double x1, x2, x3;
+            x1 = persamaan[0] * persamaan[0];
+            x2 = (persamaan[1] * persamaan[0]) + (persamaan[0] * persamaan[1]);
+            x3 = persamaan[1] * persamaan[1];
+            return (x1,x2,x3);
+        }
+    }
+}
